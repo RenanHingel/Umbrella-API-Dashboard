26513e380f738afb51a5f41a80d009f23ab59b2a
RenanHingel <renanhingel@gmail.com>
2021-07-07 08:23:11 -0300

    Update details.py
    Improved the details.py file to simplify user management, the code will now convert the API Key info to BASE64

--- controller/details.py
+++ controller/details.py
@@ -1,2 +1,9 @@
-management_base64 = 'Basic YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY'
-org_id = "xxxxxxx"
+################################################################
+# Edit the following variables with your Umbrella Dashboard information
+#
+# Check https://developer.cisco.com/docs/cloud-security/#!getting-started if you don't have your API Key & Secret yet!
+################################################################
+org_id = "XXXXXX"
+api_key = "YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY"
+api_secret = "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ"
+################################################################
